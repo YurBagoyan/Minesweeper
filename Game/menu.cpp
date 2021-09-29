2c03105237de843cb4a287a57927a8f2c6e41bb7
YurBagoyan <yurbagoyan@gmail.com>
2021-09-29 21:29:32 +0400

    Adding menu.cpp

---
+++ Game/menu.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include "input.hpp"
+
+void show_menu(std::string Menu_Rows[], int const Menu_Count, int const select)
+{
+    system("clear");
+    
+    for(int i = 0; i < Menu_Count; ++i)
+    {
+        if(i == select)
+        {
+            std::cout << "> " << Menu_Rows[i] << "\n";
+        }
+        else
+        {
+            std::cout << "  " << Menu_Rows[i] << "\n";
+        }
+    }
+}   
+
+int main()
+{
+
+    const int Menu_Count = 5;
+    std::string Menu_Rows[Menu_Count] = { "Start", "Records", "Options", "About", "Exit" };
+
+    int select = 0;
+    show_menu(Menu_Rows, Menu_Count, select);
+    
+    for(char key = 0; key != 27; )
+    {
+        cbreak();
+        key = keypress();
+        normal();
+
+        switch(key)
+        {
+            case 'w': 
+                if(select == 0)
+                {
+                    select = 4;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                else
+                {
+                    --select;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                break;
+            
+            case 'W':
+                if(select == 0)
+                {
+                    select = 4;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                else
+                {
+                    --select;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                break;
+
+
+            case 's':
+                if(select == 4)
+                {
+                    select = 0;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                else
+                {
+                    ++select;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                break;
+
+            case 'S':
+                if(select == 4)
+                {
+                    select = 0;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                else
+                {
+                    ++select;
+                    show_menu(Menu_Rows, Menu_Count, select);
+                }
+                break;
+        }
+    }
+}
